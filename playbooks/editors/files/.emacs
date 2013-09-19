44580319534c9d616d9dc52264b107ceced513be
Thomas Franquelin <ostapneko@gmail.com>
2013-09-19 17:58:42 +0100

    More Emacs conf

--- playbooks/editors/files/.emacs
+++ playbooks/editors/files/.emacs
@@ -1,12 +1,18 @@
-;; Package installation
+;; Packages
 (load-file "~/.emacs.d/package-installation.el")
+
+;; Load basic packages
+(check-packages '(twillight-theme
+		  jujube-theme
+		  yaml-mode
+		  magit
+		  auto-complete))
 
 ;; Theme
 (custom-set-variables
- '(custom-safe-themes (quote ("6cfe5b2f818c7b52723f3e121d1157cf9d95ed8923dbc1b47f392da80ef7495d" default))))
-(custom-set-faces)
+ '(custom-safe-themes (quote ("579e9950513524d8739e08eae289419cfcb64ed9b7cc910dd2e66151c77975c4" "6cfe5b2f818c7b52723f3e121d1157cf9d95ed8923dbc1b47f392da80ef7495d" default))))
 
-(load-theme 'twilight t)
+(load-theme 'jujube t)
 
 ;; Move mode
 (load-file "~/.emacs.d/move-mode.el")
@@ -24,3 +30,16 @@
 ;; key chords
 (key-chord-mode 1)
 (key-chord-define-global "jk" 'enable-move-mode)
+
+;; Tab hatred
+(setq tab-width 2)
+(setq indent-tabs-mode nil)
+
+;; Line numbers
+(require 'linum)
+(add-hook 'find-file-hook (lambda () (linum-mode 1)))
+;; Add space between line number and text
+(setq linum-format "%3d ")
+
+;; Haskell
+(load-file "~/.emacs.d/haskell.el")
---
+++ playbooks/editors/files/move-mode.el
@@ -0,0 +1,81 @@
+(check-packages '(key-chord))
+(defun duplicate-current-line (&optional n)
+  "duplicate current line, make more than 1 copy given a numeric argument"
+  (interactive "p")
+  (save-excursion
+    (let ((nb (or n 1))
+          (current-line (thing-at-point 'line)))
+      ;; when on last line, insert a newline first
+      (when (or (= 1 (forward-line 1)) (eq (point) (point-max)))
+        (insert "\n"))
+
+      ;; now insert as many time as requested
+      (while (> n 0)
+             (insert current-line)
+             (decf n)))))
+
+(defun kill-whole-line ()
+  "Delete a while line, including the newline"
+  (interactive)
+  (move-beginning-of-line nil)
+  (kill-line 1))
+
+(defun add-new-line ()
+  "Add a new line and disable move mode"
+  (interactive)
+  (move-end-of-line nil)
+  (newline-and-indent)
+  (disable-move-mode))
+
+(defun add-new-line-above ()
+  "Add a new line above, move the cursor up and disable move mode"
+  (interactive)
+  (move-beginning-of-line nil)
+  (newline)
+  (disable-move-mode)
+  (previous-line)
+  (indent-for-tab-command))
+
+(define-minor-mode move-mode
+  "Move mode"
+  ;; The initial value - Set to 1 to enable by default
+  1
+  ;; The indicator for the mode line.
+  " Move"
+  ;; The minor mode keymap
+  `(
+    ("s" . next-line)
+    ("w" . previous-line)
+    ("a" . backward-char)
+    ("d" . forward-char)
+    ("\S-d" . forward-word)
+    ("\S-a" . backward-word)
+    ("\C-s" . save-buffer)
+    ("o" . find-file)
+    ("\C-l" . duplicate-current-line)
+    ("\M-l" . kill-whole-line)
+  )
+  ;; MAKE mode global rather than buffer local
+  :global 1
+)
+
+;; Need to move those out of the keymap
+;; because the call 'move-mode recursively and
+;; Emacs doesn't look happy with it
+(define-key move-mode-map (kbd "j") 'disable-move-mode)
+(define-key move-mode-map (kbd "l") 'add-new-line)
+(define-key move-mode-map (kbd "L") 'add-new-line-above)
+
+(add-hook 'minibuffer-setup-hook 'disable-move-mode)
+(add-hook 'minibuffer-exit-hook 'enable-move-mode)
+(add-hook 'term-mode-hook 'disable-move-mode)
+
+(defun disable-move-mode ()
+  "Exit move mode"
+  (interactive)
+  (move-mode 0))
+
+(defun enable-move-mode ()
+  "Enter move mode"
+  (interactive)
+  (move-mode 1))
